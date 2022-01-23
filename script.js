7d7c5ed29f590fe03410af00c7d7e026386b34c1
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-01-23 19:43:20 -0300

    hora
    novo

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img= window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()
-//var hora = 19
+//var hora = data.getHours()
+var hora = 13
 msg.innerHTML = `Agora são ${hora} horas.`
 if(hora >= 0 && hora < 12) {
     //Bom Dia!
     img.src = 'imagens/manha.png'
     document.body.style.background ='#e7e883'
-} else  if (hora >= 12 && hora <= 18) {
+} else  if (hora <= 18) {
     //Boa tarde!
     img.src = 'imagens/foto.png'
     document.body.style.background = '#dac34e'
